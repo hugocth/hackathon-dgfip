571e5c9aedcdcdf662d232c3b142fdf4522b4460
Hugo Chan-To-Hing <hugo.cth@student-cs.fr>
2022-04-30 02:23:37 +0200

    First commit for Hugo

--- utils.py
+++ utils.py
@@ -5,6 +5,10 @@ import json
 
 
 def API_to_df(dataset_id,format="json"):
+    """
+    Input: dataset_id from data.gouv and target format
+    Output: pandas or geopandas dataframe
+    """
     response_API = requests.get(f"https://data.economie.gouv.fr/api/v2/catalog/datasets/{dataset_id}/exports/{format}")
     data = response_API.json()
     if format == "geojson":
@@ -18,6 +22,14 @@ def API_to_df(dataset_id,format="json"):
         df = pd.read_json(f"data/{dataset_id}.json")
         return df
 
+
+def values(df, key):
+  """To get the possible values for a column of a dataframe"""
+  l1 = list(df[key].to_numpy())
+  l2 = []
+  for elem in l1 :
+    l2.append(elem.split(',')[0])
+  return set(l2)
 
 
 if __name__ == "__main__":
